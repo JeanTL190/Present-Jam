709f52a4027944eef3d52c1776b508847cb49049
grodriguesc <groodrigues17@gmail.com>
2019-12-17 17:51:32 -0300

    Inimigo segue, toma dano e morre

---
+++ A Christmas Gift/Assets/Scripts/EnemyController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+
+public float enemyHealth;
+public float enemyDamage;
+public float damageTaken;
+
+public PlayerController player;
+
+public void Attack(){
+
+    ///TODO: Spawnar animação de ataque e tirar vida do player
+
+}
+
+private void OnTriggerEnter(Collider other) {
+    if(other.tag == "Player"){
+
+        Attack();
+
+    }else if( other.tag == "PlayerAttack"){
+
+        enemyHealth -= damageTaken;
+
+    }
+}
+
+public void Die(){
+
+    if(enemyHealth <= 0)
+    {
+        Destroy(this.GameObject);
+    }
+
+}
+
+}
