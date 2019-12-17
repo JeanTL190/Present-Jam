4faed22bbffa4c1df01fe24db805000f8c23136a
Jean Tan Li <chines1998@hotmail.com>
2019-12-17 16:41:55 -0300

    Noel Movimentação

---
+++ A Christmas Gift/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    [SerializeField] private Transform target;
+
+    public float smoothSpeed = 0.125f;
+    private Vector3 offset;
+    void Start()
+    {
+        offset = transform.position - target.position;
+    }
+    private void FixedUpdate()
+    {
+        Vector3 desiredPosition = target.position + offset;
+        Vector3 SmoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
+
+        transform.position = SmoothedPosition;
+
+    }
+}
