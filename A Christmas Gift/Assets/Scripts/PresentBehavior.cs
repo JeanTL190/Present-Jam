3f483eec72bfcc0cd9562b04091f2314c6677f34
Jean Tan Li <chines1998@hotmail.com>
2019-12-18 11:33:45 -0300

    Present Spawn and Behavior

---
+++ A Christmas Gift/Assets/Scripts/PresentBehavior.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PresentBehavior : MonoBehaviour
+{
+    [SerializeField] float timeLife = 10f;
+    [SerializeField] int health = 50;
+    void Start()
+    {
+        StartCoroutine("Sobreviveu");
+    }
+
+    void Update()
+    {
+
+    }
+
+    private void TookDamage()
+    {
+        if (health <= 0)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+    IEnumerator Sobreviveu() {
+        yield return new WaitForSeconds(timeLife);
+        if (health > 0)
+        {
+            // Pontua
+            Destroy(this.gameObject);
+        }
+    }
+}
---
+++ A Christmas Gift/Assets/Scripts/PresentSpawn.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PresentSpawn : MonoBehaviour
+{
+    [SerializeField] float maxX;
+    [SerializeField] float maxY;
+    [SerializeField] float minX;
+    [SerializeField] float minY;
+    [SerializeField] float timeSpawn=5f;
+    [SerializeField] GameObject[] presentes;
+
+    private void Start()
+    {
+        StartCoroutine("Spawn");
+    }
+
+    IEnumerator Spawn()
+    {
+        while (true) {
+            yield return new WaitForSeconds(timeSpawn);
+            GameObject instacia = presentes[Random.Range(0, presentes.Length - 1)];
+            instacia.transform.position = new Vector2(Random.Range(minX, maxX), Random.Range(minY, maxY));
+
+            Instantiate(instacia);
+        }
+    }
+}
